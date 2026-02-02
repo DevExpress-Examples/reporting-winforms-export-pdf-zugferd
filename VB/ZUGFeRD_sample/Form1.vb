b1d4204e6a378615918fa0883a55f9696e7b13b9
DevExpressExampleBot <fakeEmail>
2026-02-03 02:45:08 +0400

    Source auto update [skip ci]

--- VB/ZUGFeRD_sample/Form1.vb
+++ VB/ZUGFeRD_sample/Form1.vb
@@ -12,52 +12,25 @@ Namespace ZUGFeRD_sample
 			InitializeComponent()
 		End Sub
 
-		' Refer to chapter 7.7 of the FACTUR-X 1.08 specification
-		Private Sub buttonExportZugferd_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ButtonExportZugferd.Click
-			Using report As New XtraReport1()
-				report.XmlDataPath = "German//xrechnung.xml"
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim report = New XtraReport1()
+			report.XmlDataPath = "ZUGFeRD-rechnung.xml"
 
-				Dim additionalMetadata As String = File.ReadAllText("German//ZUGFeRD_DocumentInfo.txt") & File.ReadAllText("German//ZUGFeRD_PdfASchema.txt")
+			Dim additionalMetadata As String = File.ReadAllText("ZUGFeRD_DocumentInfo.txt") & File.ReadAllText("ZUGFeRD_PdfASchema.txt")
 
-				Dim options As New PdfExportOptions() With {
-					.PdfACompatibility = PdfACompatibility.PdfA3b,
-					.AdditionalMetadata = additionalMetadata
-				}
+			Dim options As New PdfExportOptions() With {
+				.PdfACompatibility = PdfACompatibility.PdfA3b,
+				.AdditionalMetadata = additionalMetadata
+			}
 
-				options.Attachments.Add(New PdfAttachment() With {
-					.FilePath = "German//xrechnung.xml",
-					.Type = "text/xml",
-					.Relationship = PdfAttachmentRelationship.Alternative,
-					.Description = "Rechnungsdaten im ZUGFeRD-XML-Format"
-				})
+			options.Attachments.Add(New PdfAttachment() With {
+				.FilePath = "ZUGFeRD-rechnung.xml",
+				.Type = "text/xml",
+				.Description = "Rechnungsdaten im ZUGFeRD-XML-Format"
+			})
 
-				report.ExportToPdf("germanResult.pdf", options)
-			End Using
-			Process.Start("germanResult.pdf")
-		End Sub
-
-		' Refer to chapter 6.2 of the FACTUR-X 1.08 specification
-		Private Sub buttonExportFacturX_Click(sender As Object, e As EventArgs) Handles ButtonExportFacturX.Click
-			Using report As New XtraReport1()
-				report.XmlDataPath = "French//factur-x.xml"
-
-				Dim additionalMetadata As String = File.ReadAllText("French//ZUGFeRD_DocumentInfo.txt") & File.ReadAllText("French//ZUGFeRD_PdfASchema.txt")
-
-				Dim options As New PdfExportOptions() With {
-					.PdfACompatibility = PdfACompatibility.PdfA3b,
-					.AdditionalMetadata = additionalMetadata
-				}
-
-				options.Attachments.Add(New PdfAttachment() With {
-					.FilePath = "French//factur-x.xml",
-					.Type = "text/xml",
-					.Relationship = PdfAttachmentRelationship.Data,
-					.Description = "Rechnungsdaten im ZUGFeRD-XML-Format"
-				})
-
-				report.ExportToPdf("frenchResult.pdf", options)
-			End Using
-			Process.Start("frenchResult.pdf")
+			report.ExportToPdf("result.pdf", options)
+			Process.Start("result.pdf")
 		End Sub
 	End Class
 End Namespace
